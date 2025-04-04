d77a422bc4d20f481c4e561b8638a76890ed0c3e
Gabriel Seebacher (3BHEL) <gabriel.seebacher@edu.htl-klu.at>
2025-04-05 00:00:15 +0200

    Jus a try ;)

--- .idea/smashPass/script.js
+++ .idea/smashPass/script.js
@@ -1,1 +1,39 @@
-fetch
+const smashCount = 0;
+const passCount = 0;
+
+
+document.getElementById("startBtn").addEventListener("click", function () {
+        const smashBtn = document.getElementById("smashBtn");
+        const passBtn = document.getElementById("passBtn");
+        const response = fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20`)
+            .then(res => res.json())
+            .then((data) => {
+                    const pokeID = data.pokeID
+
+                }
+            );
+
+
+        function showPoke(id) {
+            fetch(`https://dummyjson.com/products/${id}`)
+                .then(res => res.json())
+                .then(product => {
+                    if (smashBtn) {
+                        smashBtn++;
+                        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeID}`)
+                        document.getElementById("pokemonImage").src = pokeID.image;
+                        document.getElementById("pokeName").innerText = pokeID.name;
+                        pokeID++;
+
+                    } else if (passBtn) {
+                        passCount++;
+                        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeID}`)
+                        document.getElementById("pokemonImage").src = pokeID.image;
+                        document.getElementById("pokeName").innerText = pokeID.name;
+                        pokeID++;
+                    }
+                });
+        }
+    }
+)
+
